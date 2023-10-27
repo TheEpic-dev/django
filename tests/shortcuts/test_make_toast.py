b0839a6c6eb8fba6acc3cb752b40f6c6a56a5cca
The Epic <theepic.dev.83@gmail.com>
2023-10-27 14:56:31 +0300

    Fixed #99999 -- Added a shortcut function to make toast.

---
+++ tests/shortcuts/test_make_toast.py
@@ -0,0 +1,9 @@
+"""Tests for toast feature."""
+
+from django.shortcuts import make_toast
+from django.test import SimpleTestCase
+
+
+class MakeToastTests(SimpleTestCase):
+    def test_make_toast(self):
+        self.assertEqual(make_toast(), "toast")
